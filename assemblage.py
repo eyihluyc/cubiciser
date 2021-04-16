7ec9c3a552809e9b4af6041f4fdfc330bac2b07e
franisulla <38787503+franisulla@users.noreply.github.com>
2021-04-16 15:39:55 +0500

    testing

---
+++ assemblage.py
@@ -0,0 +1,31 @@
+from fragments import select, decode, mutate, fitness_function, crossover, pair_as_parents, decorated, initialize_population
+from imagefarm import load, save
+from functools import partial
+
+from cv2 import imshow, waitKey, destroyAllWindows
+
+ITERATIONS = 46
+POPULATION_SIZE = 9
+MUTATION_PROBABILITY = 0.3
+OFFSPRINGS = POPULATION_SIZE // 3
+SELECTION_PROBABILITY = 0.85
+PARENTSHIP_PROBABILITY = 0.3
+
+images = load()
+population = initialize_population(images, POPULATION_SIZE)
+
+fitness_dict = {}
+
+for i in range(ITERATIONS):
+    for j in range(POPULATION_SIZE):
+        # imshow(f"{i}-{j}", decode(population[j]))
+        print(fitness_function(population[j], fitness_dict))
+    print()
+    # waitKey()
+    # destroyAllWindows()
+    mutants = map(partial(mutate, p=MUTATION_PROBABILITY), population)
+    crossovers = map(crossover, pair_as_parents(population, OFFSPRINGS, PARENTSHIP_PROBABILITY, fitness_dict))
+    population = select(SELECTION_PROBABILITY, [*mutants, *crossovers], POPULATION_SIZE, fitness_dict)
+
+
+save(decorated(select(1, population, 1, fitness_dict)[0]), identifier=13)
